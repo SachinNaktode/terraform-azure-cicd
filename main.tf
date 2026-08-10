ab9cfdf4e736d79ed85e5fb1851e99f3dfb28c5a
Sachin Naktode <sachin.naktode.ext@constl.com>
2026-08-11 02:36:33 +0530

    Improve storage replication security

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "azurerm_storage_account" "storage" {
   location            = azurerm_resource_group.rg.location
 
   account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_replication_type = "GRS"
 
   min_tls_version                 = "TLS1_2"
   public_network_access_enabled   = false
